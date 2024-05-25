64669920980a89c11d5c686234fa03fd4b8dff61
Ilnur <ilnur.manapov@yandex.ru>
2024-05-26 02:38:39 +0300

    update modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-//import {TonConnectButton} from "@tonconnect/ui-react";
-//import {AddressInfo} from "./AddressInfo";
-//import {Settings} from "./Settings";
-//import {BackendTokenContext} from "./BackendTokenContext";
-//import {useState} from "react";
-//import {SendTx} from "./SendTx";
-//import {BackendDemoApi} from "./BackendDemoApi";
 import Header from './components/header/Header.jsx'
 import Spider from './components/spider/Spider.jsx'
 import Presale from './components/presale/Presale.jsx'
 import Noteligible from './components/noteligible/Noteligible.jsx'
 import data from './data.js'
-//import { useState } from 'react'
 import { useTonAddress } from '@tonconnect/ui-react';
+import React from 'react'
 
 function SuccessWallet(){
   const address = useTonAddress();
@@ -24,27 +17,7 @@ function SuccessWallet(){
 }
 
 function App() {
-
-  /*const address = useTonAddress();
-
-  const wallets = users.map((wallet) => {
-        if (wallet.id === address) {
-          return <Presale wallet={wallet} key={wallet.id}/>;
-        }
-    })*/
-    
-  //const [token, setToken] = useState<string | null>(null);
-
   return (
-      //<BackendTokenContext.Provider value={{token, setToken}}>
-      //  <div>
-      //    <header style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '20px' }}>
-      //      <Settings />
-      //      <TonConnectButton />
-      //    </header>
-      //      <SendTx />
-      //  </div>
-      //</BackendTokenContext.Provider>
       <div>
           <Header />
           <Spider />
@@ -53,11 +26,5 @@ function App() {
       
   )
 }
-//<div style={{ height: '140px' }}>
-//                <AddressInfo />
-//                <WalletInfo />
-//            </div>
-//<BackendDemoApi />
-
 
 export default App
