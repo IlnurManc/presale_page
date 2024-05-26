d57a4418f4e7eb1a5355fdfdec9f97555a23abf1
Ilnur <ilnur.manapov@yandex.ru>
2024-05-26 21:48:41 +0300

    update connect button

--- src/components/connect/Connect.jsx
+++ src/components/connect/Connect.jsx
@@ -1,15 +1,17 @@
 import './connect.css'
+import {useTonWallet, useTonConnectUI} from "@tonconnect/ui-react";
 import {TonConnectButton} from "@tonconnect/ui-react";
 
 const Connect = () => {
+    const [tonConnectUI] = useTonConnectUI();
+    const wallet = useTonWallet();
+    const onClick = async () => {
+        if (!wallet) {
+            tonConnectUI.connectWallet();
+        }}
     return (  
-        /*<section className="connect">
-            <div className="connect__title">
-                Connect your wallet
-            </div>
-        </section>*/
         <section className="connect">
-            <button className="connect__button" >
+            <button className="connect__button" onClick={onClick}>
                 Connect Wallet
             </button>
         </section>
--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import {TonConnectUIProvider} from "@tonconnect/ui-react";
+import { THEME } from '@tonconnect/ui';
 import App from './App.tsx'
 import "./styles/reset.css"
 import "./styles/main.css"
-//import WebApp from '@twa-dev/sdk'
-
-//WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  //<React.StrictMode>
   <BrowserRouter>
       <TonConnectUIProvider
           manifestUrl="https://s3.timeweb.com/301e4ceb-a1ef5c23-eed7-4903-86d7-94f30690ac2e/tonconnect/tonconnect.json"
           language="en"
-          uiPreferences={{ theme: 'SYSTEM' }}
+          uiPreferences={{ theme: THEME.LIGHT }}
       >
         <App />
       </TonConnectUIProvider>
   </BrowserRouter>
-  //</React.StrictMode>
 )
